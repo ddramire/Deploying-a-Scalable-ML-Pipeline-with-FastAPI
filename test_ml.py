46786d0ea0302d50965af9e34cfaa43116a6dedf
Dora Munguia <darcelymunguia@gmail.com>
2026-02-23 05:54:53 +0000

    Completed Step 4 Unit tests and screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,47 @@
 import pytest
 # TODO: add necessary import
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from ml.model import train_model, inference, compute_model_metrics
+
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model_returns_model():
     """
-    # add description for the first test
+    Test that train_model returns a trained LogisticRegression model.
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 1, 1, 0])
+
+    model = train_model(X, y)
+
+    assert isinstance(model, LogisticRegression)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_inference_returns_correct_length():
     """
-    # add description for the second test
+    Test that inference returns predictions of the correct length.
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 1, 1, 0])
+
+    model = train_model(X, y)
+    preds = inference(model, X)
+
+    assert len(preds) == len(X)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics_range():
     """
-    # add description for the third test
+    Test that compute_model_metrics returns values between 0 and 1.
     """
-    # Your code here
-    pass
+    y_true = np.array([0, 1, 1, 0])
+    y_pred = np.array([0, 1, 0, 0])
+
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
